192971a4f0816e186d98b55f365663d4c8e10ae9
Hudziol <hudziolxd@gmail.com>
2022-09-19 12:15:29 +0200

    190920221215

--- jquery 1/js/app.js
+++ jquery 1/js/app.js
@@ -1,5 +1,15 @@
-$(document).ready(()=>{
-    $("#btn-hello").click(()=>{
+$(document).ready(() => {
+    $("#btn-hello").click(() => {
         alert("Udało się!");
+    });
+
+    $("#btn-show").click(() => {
+        $("#offer-box").show();
+    });
+    $("#btn-hide").click(() => {
+        $("#offer-box").hide();
+    });
+    $("#btn-toggle").click(() => {
+        $("#offer-box").toggle();
     });
 });
